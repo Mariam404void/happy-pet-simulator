6d858424601dad8b89ef923bbacf346f57cd8521
Mariam Khokhonishvili <my@mail.com>
2025-07-31 01:49:01 +0400

    init-game-script

---
+++ happy-pet-simulator.py
@@ -0,0 +1,12 @@
+pet_name = "Mochi"
+happiness = 5
+
+while True:
+    print(f"\nWhat would you like to do with {pet_name}?")
+    print("1. Feed")
+    print("2. Pet")
+    print("3. Check happiness")
+    print("4. Exit")
+
+    choice = input("Choose number between 1-4: ")
+
